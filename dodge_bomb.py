64cf3c860a8bf2bfe5a8e0cf64ef0144fe55dcad
tera <c0a2310659@edu.teu.ac.jp>
2024-04-23 14:48:05 +0900

    練習2:bom

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,9 +1,10 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1200, 600
 
 DELTA = {                          #移動量辞書
     pg.K_UP:(0,-5),
@@ -23,6 +24,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    bd_img = pg.Surface((20,20))
+    bd_img.set_colorkey((0,0,0,))
+    pg.draw.circle(bd_img,(255,0,0),(10,10),10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)    
+    vx,vy = +5,+5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -48,6 +56,8 @@ def main():
          #   sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bd_rct.move_ip(vx,vy)
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
